e884c8801f1855b25d2368240cd845347abaab5f
Jasiel Fernandez <jasielf12@gmail.com>
2020-05-12 10:19:54 -0500

    redo

--- src/pages/landing-page.pages.jsx
+++ src/pages/landing-page.pages.jsx
@@ -8,7 +8,7 @@ class LandingPage extends React.Component {
     return(
       <div className='landing-page-container'>
         <div className='header'>
-        <Navbar collapseOnSelect expand="lg">
+        <Navbar collapseOnSelect expand="lg" sticky='top'>
           <Navbar.Brand href="#home" className='nav'>Brook & Bay Grooming</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
@@ -22,30 +22,25 @@ class LandingPage extends React.Component {
             </Navbar.Collapse>
           </Navbar>
           <Container fluid='true'>
-  <Row>
-    <Col className='column-one' md>
+            <Row>
+              <Col className='column-one' md>
+                <div className='column-one-content'>
+                    <div className='column-one-text'>
+                      Brook &<br/>Bay Grooming
+                    </div>
+                    <div className='description'>
+                      Welcome to Brook and Bay Grooming! I'm Brooklyn, I graduated from Nash Grooming Academy in the spring of 2020. I'm certified in canine CPR & safety. I'm proud to say I was a top student and was invited to attend a grooming competition in Australia. However, I was excited to return home and start grooming my furry friends in Owensboro. Give me a call and book your appointment today! 
+                    </div>
+                </div>
 
-  <div className='column-one-content'>
-      <div className='column-one-text'>
-        Brook &<br/>Bay Grooming
-      </div>
-      <div className='description'>
-        Welcome to Brook and Bay Grooming! I'm Brooklyn, I graduated from Nash Grooming Academy in the spring of 2020. I'm certified in canine CPR & safety. I'm proud to say I was a top student and was invited to attend a grooming competition in Australia. However, I was excited to return home and start grooming my furry friends in Owensboro. Give me a call and book your appointment today! 
-      </div>
-  </div>
-
-    </Col>
-    <Col md className='column-two'>
-      <div className='image-container'>
-      <Image src={ mainpage } fluid/>
-      </div>
+              </Col>
+              
+              <Col md className='column-two'>
       
-      
-
-    </Col>
-  </Row>
+              </Col>
+            </Row>
   
-</Container>
+          </Container>
         </div>
       </div>
     )
